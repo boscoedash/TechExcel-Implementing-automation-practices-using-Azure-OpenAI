9aa223f8d2c0f11edbb4e38e66023de6dc374688
bhlokana@microsoft.com <bhlokana@microsoft.com>
2024-03-21 09:55:55 -0500

    init

--- src/ContosoSuitesDashboard/pages/2_Call_Center.py
+++ src/ContosoSuitesDashboard/pages/2_Call_Center.py
@@ -32,28 +32,27 @@ language_key = config['LanguageKey']
 ### Exercise 05: Provide live audio transcription
 def create_transcription_request(audio_file, speech_key, speech_region, speech_recognition_language="en-US"):
     # Create an instance of a speech config with specified subscription key and service region.
-    # speech_config = ...
-    # TODO: set speech recognition language
+
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_region)
+    speech_config.speech_recognition_language=speech_recognition_language
 
     # Prepare audio settings for the wave stream
     channels = 1
     bits_per_sample = 16
     samples_per_second = 16000
 
-    # TODO: Create audio configuration using the push stream
-    # wave_format = ...
-    # stream = ...
-    # audio_config = ...
+    # Create audio configuration using the push stream
+    wave_format = speechsdk.audio.AudioStreamFormat(samples_per_second, bits_per_sample, channels)
+    stream = speechsdk.audio.PushAudioInputStream(stream_format=wave_format)
+    audio_config = speechsdk.audio.AudioConfig(stream=stream)
 
-    # TODO: use the ConversationTranscriber class to create a transcriber object
-    # transcriber = ...
-
+    transcriber = speechsdk.transcription.ConversationTranscriber(speech_config, audio_config)
     all_results = []
-    done = False
 
-    # Callback events for transcribed and stopped/cancelled states
     def handle_final_result(evt):
         all_results.append(evt.result.text)
+
+    done = False
 
     def stop_cb(evt):
         print('CLOSING on {}'.format(evt))
@@ -61,34 +60,41 @@ def create_transcription_request(audio_file, speech_key, speech_region, speech_r
         done= True
 
     # Subscribe to the events fired by the conversation transcriber
-    # TODO: add event handlers for transcribed, session_started, session_stopped, and canceled events
+    transcriber.transcribed.connect(handle_final_result)
+    transcriber.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    transcriber.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    transcriber.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous transcription on either session stopped or canceled events
+    transcriber.session_stopped.connect(stop_cb)
+    transcriber.canceled.connect(stop_cb)
 
-    # Begin the transcription process
-    # transcriber.start_transcribing_async()
+    transcriber.start_transcribing_async()
 
     # Read the whole wave files at once and stream it to sdk
     _, wav_data = wavfile.read(audio_file)
-    # TODO: write the stream to bytes and close it when done
+    stream.write(wav_data.tobytes())
+    stream.close()
     while not done:
         time.sleep(.5)
 
-    # transcriber.stop_transcribing_async()
+    transcriber.stop_transcribing_async()
     return all_results
 
-def create_live_transcription_request(speech_key, speech_region, speech_recognition_language="en-US"):
-    # Create an instance of a speech config with specified subscription key and service region.
-    # speech_config = ...
-    # TODO: set speech recognition language
-    # TODO: use the ConversationTranscriber class to create a transcriber object
-    # transcriber = ...
 
-    all_results = []
+def create_live_transcription_request(speech_key, speech_region, speech_recognition_language="en-US"):
+
+    # Creates speech configuration with subscription information
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_region)
+    speech_config.speech_recognition_language=speech_recognition_language
+    transcriber = speechsdk.transcription.ConversationTranscriber(speech_config)
+
     done = False
 
-    # Callback events for transcribed and stopped/cancelled states
     def handle_final_result(evt):
         all_results.append(evt.result.text)
         print(evt.result.text)
+
+    all_results = []
 
     def stop_cb(evt: speechsdk.SessionEventArgs):
         """callback that signals to stop continuous transcription upon receiving an event `evt`"""
@@ -97,10 +103,15 @@ def create_live_transcription_request(speech_key, speech_region, speech_recognit
         done = True
 
     # Subscribe to the events fired by the conversation transcriber
-    # TODO: add event handlers for transcribed, session_started, session_stopped, and canceled events
+    transcriber.transcribed.connect(handle_final_result)
+    transcriber.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    transcriber.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    transcriber.canceled.connect(lambda evt: print('CANCELLED {}'.format(evt)))
+    # stop continuous transcription on either session stopped or canceled events
+    transcriber.session_stopped.connect(stop_cb)
+    transcriber.canceled.connect(stop_cb)
 
-    # Begin the transcription process
-    # transcriber.start_transcribing_async()
+    transcriber.start_transcribing_async()
 
     # Streamlit refreshes the page on each interaction,
     # so a clean start and stop isn't really possible with button presses.
@@ -114,35 +125,52 @@ def create_live_transcription_request(speech_key, speech_region, speech_recognit
 
     return
 
+
 def make_azure_openai_chat_request(system, call_contents):
     # Create an Azure OpenAI client.
+    client = openai.AzureOpenAI(
+        base_url=f"{aoai_endpoint}/openai/deployments/{deployment_name}/",
+        api_key=aoai_api_key,
+        api_version="2023-12-01-preview"
+    )
 
     # Create and return a new chat completion request
-    # There should be two messages, one from role=system with the system prompt,
-    # and one from role=user with the call contents.
-    raise NotImplementedError
+    return client.chat.completions.create(
+        model=deployment_name,
+        messages=[
+            {"role": "system", "content": system},
+            {"role": "user", "content": call_contents}
+        ],
+    )
+
 
 def is_call_in_compliance(call_contents, include_recording_message, is_relevant_to_topic):
-    # call_contents comes in as a list of strings, so join them together with spaces
-    # If the user has checked the include_recording_message or is_relevant_to_topic boxes,
-    # add the appropriate prompt:
-        # "2. Was the caller aware that the call was being recorded?"
-        # "3. Was the call relevant to the hotel and resort industry?"
+    joined_call_contents = ' '.join(call_contents)
+    if include_recording_message:
+        include_recording_message_text = "2. Was the caller aware that the call was being recorded?"
+    else:
+        include_recording_message_text = ""
+
+    if is_relevant_to_topic:
+        is_relevant_to_topic_text = "3. Was the call relevant to the hotel and resort industry?"
+    else:
+        is_relevant_to_topic_text = ""
 
     system = f"""
         You are an automated analysis system for Contoso Suites. Contoso Suites is a luxury hotel and resort chain with locations
         in a variety of Caribbean nations and territories.
-        
+            
         You are analyzing a call for relevance and compliance.
 
         You will only answer the following questions based on the call contents:
         1. Was there vulgarity on the call?
-        TODO: finish this based on the prompts above!
+        {include_recording_message_text}
+        {is_relevant_to_topic_text}
     """
 
-    # Call make_azure_openai_chat_request
-    # Return the message content for the response's first choice
-    raise NotImplementedError
+    response = make_azure_openai_chat_request(system, joined_call_contents)
+    return response.choices[0].message.content
+
 
 
 ### Exercise 06: Generate call summaries
@@ -352,25 +380,31 @@ def main():
 
     st.write("## Simulate a call")
 
-    # TODO: Add a file uploader to the Streamlit app
-    # TODO: Add a conditional based on:
-    #    - If there is an uploaded file
-    #    - And either 'file_transcription' is not in st.session_state or st.session_state.file_transcription is False
-    # If so:
-        # TODO: Add an st.audio() element to the Streamlit app to play the audio back
-        # TODO: Use st.spinner() to wrap the transcription
-        # TODO: Call the create_transcription_request() function and save its results to session state as file_transcription_results
-        # TODO: Set file_transcription to True in session state
-        # TODO: If there is an uploaded file, call st.success() to indicate that the transcription is complete
+    uploaded_file = st.file_uploader("Upload an audio file", type="wav")
+    if uploaded_file is not None and ('file_transcription' not in st.session_state or st.session_state.file_transcription is False):
+        st.audio(uploaded_file, format='audio/wav')
+        with st.spinner("Transcribing the call..."):
+            all_results = create_transcription_request(uploaded_file, speech_key, speech_region)
+            st.session_state.file_transcription_results = all_results
+            st.session_state.file_transcription = True
+        st.success("Transcription complete!")
 
     if 'file_transcription_results' in st.session_state:
         st.write(st.session_state.file_transcription_results)
+
         
     st.write("## Perform a Live Call")
 
-    # TODO: Add a Streamlit Extras button called start_recording and whose label is Record.
-    # Streamlit Extras buttons also need a key, which you can name something like "recording_in_progress".
-    # TODO: If the button is clicked, create a spinner and then call create_live_transcription_request().
+    start_recording = stx.button("Record", key="recording_in_progress")
+    if start_recording:
+        with st.spinner("Transcribing your conversation..."):
+            create_live_transcription_request(speech_key, speech_region)
+
+    if 'transcription_results' in st.session_state:
+        st.write(st.session_state.transcription_results)
+    else:
+        print("Nothing in transcription results!")
+
 
     if 'transcription_results' in st.session_state:
         st.write(st.session_state.transcription_results)
@@ -393,13 +427,19 @@ def main():
     include_recording_message = st.checkbox("Call needs an indicator we are recording it")
     is_relevant_to_topic = st.checkbox("Call is relevant to the hotel and resort industry")
 
-    # TODO: Add a Streamlit button to check for compliance. If the button is clicked:
-       # TODO: Use st.spinner() to wrap the compliance check
-       # TODO: Set call_contents to either file_transcription_results or transcription_results, depending on
-       # which has content. If neither has content, write out an error message for the user.
-       # TODO: Make a call to is_call_in_compliance and write out the results using st.write()
-       # TODO: Call st.success() to indicate that the compliance check is complete
-    
+    if st.button("Check for Compliance"):
+        with st.spinner("Checking for compliance..."):
+            if 'file_transcription_results' in st.session_state:
+                call_contents = st.session_state.file_transcription_results
+            elif 'transcription_results' in st.session_state:
+                call_contents = st.session_state.transcription_results
+            else:
+                st.write("Please upload an audio file or record a call before checking for compliance.")
+            if call_contents is not None and len(call_contents) > 0:
+                compliance_results = is_call_in_compliance(call_contents, include_recording_message, is_relevant_to_topic)
+                st.write(compliance_results)
+        st.success("Compliance check complete!")
+
     # Exercise 6: Generate call summaries
     st.write("## Generate call summaries")
 
